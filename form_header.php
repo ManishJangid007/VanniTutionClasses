b9e4c4216e74346a63fb016ff9a60212c22ef8c6
Manish <anitasharma4440@gmail.com>
2022-01-10 05:08:15 -0800

    Login Page added

--- form_header.php
+++ form_header.php
@@ -25,15 +25,19 @@
     <link href="style.css" rel="stylesheet"/>
     <title>Vaani Tution Point</title>
   </head>
-  <body style="font-family: bahnschrift; background-image: url('assets/Animated Shape.svg')">
+  <body style="font-family: bahnschrift">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
-    
+     
+  
+  
+  
+  
+  
+  <!-- ========== Start navigation bar ========== -->
 
-    <!-- ========== Start navigation bar ========== -->
-
-<nav class="navbar navbar-expand-lg navbar-dark bg-primary sticky-lg-top">
+    <nav class="navbar navbar-expand-lg navbar-light bg-white sticky-lg-top">
   <div class="container-fluid" style="margin-left: 1%;">
-    <a class="navbar-brand" href="#">Hi, there</a>
+    <a class="navbar-brand" href="index.php">Vaani Tution Classes</a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
@@ -48,45 +52,10 @@
         <li class="nav-item">
           <a class="nav-link" href="#footer">Contact Us</a>
         </li>
-        <li class="nav-item">
-          <a class="nav-link disabled">Assignment</a>
-        </li>
+        
       </ul>
-
-    <!-- ========== Start login & sign up buttons ========== -->
-
-    <a style="text-decoration: none;" href="login_form.php">
-
-      <button1 style="background-color: black; margin-right: 5%; max-width: fit-content;" >
-  <div class="svg-wrapper-1">
-    <div class="svg-wrapper">
-      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24">
-        <path fill="none" d="M0 0h24v24H0z"></path>
-        <path fill="currentColor" d="M1.946 9.315c-.522-.174-.527-.455.01-.634l19.087-6.362c.529-.176.832.12.684.638l-5.454 19.086c-.15.529-.455.547-.679.045L12 14l6-8-8 6-8.054-2.685z"></path>
-      </svg>
-    </div>
   </div>
-  <span>Login</span>
-</button1>
-      </a>
-<br>
-      <a style="text-decoration: none;" href="">
-      <button1 class="bg-success" style="margin-right: 2%; max-width: fit-content;">
-  <div class="svg-wrapper-1">
-    <div class="svg-wrapper">
-      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24">
-        <path fill="none" d="M0 0h24v24H0z"></path>
-        <path fill="currentColor" d="M1.946 9.315c-.522-.174-.527-.455.01-.634l19.087-6.362c.529-.176.832.12.684.638l-5.454 19.086c-.15.529-.455.547-.679.045L12 14l6-8-8 6-8.054-2.685z"></path>
-      </svg>
-    </div>
   </div>
-  <span>Signup</span>
-</button1>
-      </a>
-
-    </div>
-  </div>
-
 <!-- ========== End navigation bar ========== -->
 
 </nav>
---
+++ login_form.php
@@ -0,0 +1,56 @@
+<?php include "form_header.php" ?>
+
+<div class="container" style="margin-top: 5%;">
+<div class="row">
+    <div class="col">
+      <img src="assets/login.png" alt="" style="width: 110%">
+    </div>
+    <div class="col">
+    
+    
+    <div class="container" style="box-shadow:
+  12.5px 13.1px 5.3px rgba(0, 0, 0, 0.04),
+  22.5px 23.4px 14.7px rgba(0, 0, 0, 0.055),
+  33.9px 35.2px 35.3px rgba(0, 0, 0, 0.068),
+  74px 77px 117px rgba(0, 0, 0, 0.1)
+; border-radius: 3%; margin-top: 20%; width: 60%">
+    
+    <br>
+    <form class="needs-validation" novalidate>
+  
+    <label for="validationCustom01" class="form-label">Username</label>
+    <input type="text" class="form-control" id="validationCustom01" placeholder="Enter Username/Email/Phone No." required style="width: 100%;">
+    <div class="valid-feedback">
+      Looks good!
+    </div>
+  
+  <br>
+  
+    <label for="validationCustom02" class="form-label">Password</label>
+    <input type="Password" class="form-control" id="validationCustom02" placeholder="Enter Password" required style="width: 100%;">
+    <div class="valid-feedback">
+      Looks good!
+    </div>
+      <br>
+  
+  <div class="col-12">
+    <button class="btn btn-primary" type="submit">Login</button>
+  </div>
+</form>
+<br>
+<a href="#" style="font-size:small;">Forgot Password</a>
+
+<br><br>
+
+
+    </div>
+    
+
+    
+
+
+
+
+    </div>
+  </div>
+</div>
